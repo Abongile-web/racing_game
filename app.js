4200aabd93e73abf1a72ab194e0831c96ba86a8c
Abongile William <abongilewilliam@gmail.com>
2021-02-22 12:29:42 +0200

    styling of components complete

---
+++ app.js
@@ -0,0 +1,11 @@
+//*****INFO BUTTON *****/
+function info() {
+    const information = document.querySelector(".information");
+    information.style.display = 'flex';
+}
+
+//*****CLOSE INFO SECTION *****/
+function close() {
+    //const information = document.querySelector(".information");
+    information.style.display = 'none';
+}
